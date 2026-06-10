dcc035b2fec7948537612eb8267991b7bc1c3abf
siddarak <daraksiddhesh@gmail.com>
2026-06-10 13:45:34 -0400

    feat: route /personal/anotherproductivitytool to child vercel app

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/personal/anotherproductivitytool",
+        destination: "https://anotherproductivitytool.vercel.app/personal/anotherproductivitytool",
+      },
+      {
+        source: "/personal/anotherproductivitytool/:path*",
+        destination: "https://anotherproductivitytool.vercel.app/personal/anotherproductivitytool/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
